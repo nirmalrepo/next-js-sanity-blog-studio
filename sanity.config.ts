8946e0449d7331070e46c9a10b2ac486f4aa3314
nirmalrepo <w.v.nirmalfernando@gmail.com>
2024-02-18 11:57:53 +1300

    Initial Commit

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,15 +2,27 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
-
+import {presentationTool} from 'sanity/presentation'
+import { locate } from './plugins/presentation/locate'
 export default defineConfig({
   name: 'default',
   title: 'GPAo Studio',
-
   projectId: 'j4vfx2it',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [
+    structureTool(),
+    visionTool(),
+    presentationTool({
+      previewUrl: {
+        draftMode: {
+          enable: 'http://localhost:3000/api/draft',
+        },
+      },
+      title:'Preview',
+      locate,
+    }),
+  ],
 
   schema: {
     types: schemaTypes,
